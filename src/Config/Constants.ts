c2a40b26c099f6198ec3b8a84e6137927f3605c0
Jonathan Corral <johnlejardinnier@gmail.com>
2022-01-26 20:36:05 +0100

    fix(config): upd api url

--- src/Config/Constants.ts
+++ src/Config/Constants.ts
@@ -1,5 +1,6 @@
 // const BASE_URL = 'https://portcamargue.frenchappweb.com'
-const BASE_URL = 'http://192.168.0.181:8001'
+// const BASE_URL = 'http://192.168.0.181:8001'
+const BASE_URL = 'https://test.frenchappweb.com'
 const API_URL = `${BASE_URL}/api/v1`
 const IMAGE_URL = `${BASE_URL}/uploads/images`
 
