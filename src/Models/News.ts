d894a47fc22081162d84d91ff36f01f1a2156b6b
Jonathan Corral <johnlejardinnier@gmail.com>
2022-01-26 20:48:05 +0100

    feat(model): create News model

---
+++ src/Models/News.ts
@@ -0,0 +1,9 @@
+export default interface News {
+    id: number
+    createdAt: number | any
+    updatedAt: number | any
+    title: string
+    body: string
+    date: Date | any
+    image: string
+}
