c3dfd9263e7114d892eed094cc3da0bbc28fe8e0
Jonathan Corral <johnlejardinnier@gmail.com>
2022-01-26 20:43:28 +0100

    fix(api/services/authentification): rename params and endpoint

--- src/Services/Api/Authentification/Login.ts
+++ src/Services/Api/Authentification/Login.ts
@@ -2,19 +2,20 @@ import api from '@/Services'
 import handleError from '@/Services/utils/handleError'
 
 interface loginParamsProps {
-    mail: string
+    email: string
     password: string
 }
 
 export default async (params: loginParamsProps) => {
-    if (!params.mail) {
+    if (!params.email) {
         return handleError({ message: 'Mail required' })
     }
     if (!params.password) {
         return handleError({ message: 'Password required' })
     }
 
-    const response = await api.post('/auth/login', params)
+    const response = await api.post('/auth/token', params)
+
 
     setTimeout(() => {
         console.log('Réponse: ', response)
