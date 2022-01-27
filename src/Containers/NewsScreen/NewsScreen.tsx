95113a0e0f9fd39ae3f4a7f511759956a9b3faf3
Jonathan Corral <johnlejardinnier@gmail.com>
2022-01-27 13:19:36 +0100

    refactor(Containers/NewsScreen): cleean code and use Component Header

--- src/Containers/NewsScreen/NewsScreen.tsx
+++ src/Containers/NewsScreen/NewsScreen.tsx
@@ -1,37 +1,44 @@
 import React, { useEffect } from 'react'
-import { SafeAreaView, View, Text, Image, TouchableOpacity, ImageBackground, FlatList } from 'react-native'
+import { SafeAreaView, View, Text, TouchableOpacity, ImageBackground, FlatList } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
-import { Common, Images, Layout } from '@/Theme'
+import { Common, Images } from '@/Theme'
 import i18n from '@/Translations'
 import { getNews } from '@/ActionCreators/NewsActionCreator'
+import { logout } from '@/ActionCreators/AuthActionCreator'
+import { Header } from '@/Components'
 
 import styles from './NewsScreenStyles'
 
-const NewsItem = ({id, title, date, image, fallback}) => (
-  <View style={[]}>
-    <TouchableOpacity onPress={() => console.log('article pressed:', id)}>
-        <ImageBackground source={{uri: image ?? fallback}} style={[styles.newsItem]} imageStyle={[{borderRadius:15}]}>
-            <View style={[styles.newsItemContent]}>
-                <Text style={[styles.newsDate]}>{date}</Text>
-                <Text style={[styles.newsTitle]}>{title}</Text>
-            </View>
-        </ImageBackground>
-    </TouchableOpacity>
-  </View>
-);
-
-const renderItem = ({item}) => (
-    <NewsItem id={item.id} title={item.title} date={item.date} image={item.image} fallback={{uri: Images.loginBackground}} />
-);
-
-const NewsScreen = (props) => {
+const NewsScreen = ({navigation}) => {
     const dispatch = useDispatch()
     const auth = useSelector(state => state.auth)
     const news = useSelector(state => state.news)
 
     const onLogout = () => {
-        props.navigation.navigate('LoginScreen')
+        dispatch(logout())
+        navigation.navigate('LoginScreen')
     }
+
+    const showNewsDetail = (id: number) => {
+        navigation.push('NewsDetailScreen', {newsId: id})
+    }
+
+    const NewsItem = ({id, title, date, image, fallback}) => (
+        <View style={[]}>
+            <TouchableOpacity onPress={() => showNewsDetail(id)}>
+                <ImageBackground source={{uri: image ?? fallback}} style={[styles.newsItem]} imageStyle={[{borderRadius:15}]}>
+                    <View style={[styles.newsItemContent]}>
+                        <Text style={[styles.newsDate]}>{date}</Text>
+                        <Text style={[styles.newsTitle]}>{title}</Text>
+                    </View>
+                </ImageBackground>
+            </TouchableOpacity>
+        </View>
+    );
+
+    const renderItem = ({item}) => (
+        <NewsItem id={item.id} title={item.title} date={item.date} image={item.image} fallback={{uri: Images.loginBackground}} />
+    );
 
     const fetchNews = () => {
         if(auth.token?.length > 0) {
@@ -47,12 +54,7 @@ const NewsScreen = (props) => {
 
     return (
         <SafeAreaView style={[Common.basicPage]}>
-            <View style={[Layout.rowWrap, {alignItems:"flex-end"}]}>
-                <TouchableOpacity style={[{flex:1}]} onPress={() => onLogout()}>
-                    <Image style={[Layout.row]} source={Images.leftArrow} />
-                </TouchableOpacity>
-                <Text style={[styles.title, {flex:10}]}>{i18n.t('news.title')}</Text>
-            </View>
+            <Header title={i18n.t('news.title')} uri={Images.logout} onPress={() => onLogout()}></Header>
             {news.news?.length > 0 && (
                 <FlatList
                     data={news.news}
